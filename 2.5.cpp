74006f91a25705eeac252f1030f22f99816bf805
Valentyn <medvediuk_m18@nuwm.edu.ua>
2021-11-16 12:31:23 +0200

    2.5

---
+++ 2.5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+int main()
+{
+
+    int userInput;
+
+    std::cout << "Enter a number: ";
+    std::cin >> ("%d", userInput);
+
+
+
+    for (int i = userInput; i <= userInput; i++)
+    {
+
+        std::cout << i + i;
+
+        return 0;
+    }
+}
