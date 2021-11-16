ecbda9afe179004e8e121cbd628f716409afda60
Valentyn <medvediuk_m18@nuwm.edu.ua>
2021-11-16 12:20:04 +0200

    1.2

---
+++ 1.2.cpp
@@ -0,0 +1,21 @@
+//**Ignore b-dy User
+
+#include <iostream>
+
+int main()
+{
+
+	//its funcional must do save a lot of characters
+	std::string name = " ";
+
+
+	//input and output information about user
+	std::cout << "What is your name?";
+
+	std::cin >> name;
+
+	std::cout << "Hello, my deary " << name << "!\n";
+
+
+}
+
